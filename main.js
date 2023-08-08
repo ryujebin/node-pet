ae05ae1a89f271e3b979d2f0a8b609e7778623fb
ryujebin <129826948+ryujebin@users.noreply.github.com>
2023-08-08 23:40:42 +0900

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const tglBtn = document.querySelector('.navbar_toggleBtn');
-const menu = document.querySelector('.navbar_menu');
-const icons = document.querySelector('.navbar_icons');
-
-tglBtn.addEventListener('click', ()=> {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-})
-
